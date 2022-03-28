820a84452b3c621ff1d1783dbadb5b1eb66187db
40324568 <rbeattie16@qub.ac.uk>
2022-03-28 11:09:14 +0100

    Created JUnit test case foR CashBox

---
+++ Sprint2_Team35/test/sprint2_team35/CashBoxTest.java
@@ -0,0 +1,20 @@
+package sprint2_team35;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CashBoxTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		CashBox testCashBox = new CashBox();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
